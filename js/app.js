263ceec32b7d9b9d89897baf9e6608491787ea0f
matmenzl <info@mathiasmenzl.ch>
2016-03-25 12:38:14 +0000

    adapted html and css to pivoted concept

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(function() {
- 
+
 // $(function(){
 //   $('#show').each(function() {
 //     $(this).
@@ -7,120 +7,94 @@ $(function() {
 // })
 
 
-//start 
-$( ".question1" ).show();
-$( ".question2" ).hide();
-$( ".question3" ).hide();
-$( ".result" ).hide();
 
 
-//question 1.1
-$('#show1').click(function(e) {
-e.preventDefault()
-$('.question2').show();
-$('.question1').hide();
-});
-
-//question 1.2
-$('#show2').click(function(e) {
-e.preventDefault()
-$('.question3').show()
-$('.question2').hide()
-});
-
-//question 1.3
-$('#show3').click(function(e) {
-e.preventDefault()
-$('.result').show()
-$('.question3').hide()
-});
-
-//result 1
-$('#show4').click(function(e) {
-e.preventDefault()
-$('.result').show()
-$('.question3').hide()
-});
-
-//question 2.1
-$('#show1').click(function(e) {
-e.preventDefault()
-$('.question4').show()
-$('.result').hide()
-});
-
-//question 2.2
-$('#show2').click(function(e) {
-e.preventDefault()
-$('.question5').show()
-$('.question4').hide()
-});
-
-//question 2.3
-$('#show3').click(function(e) {
-e.preventDefault()
-$('.question6').show()
-$('.question5').hide()
-});
-
-//result 2
-$('#show4').click(function(e) {
-e.preventDefault()
-$('.result2').show()
-$('.question6').hide()
+$(function start () {
+  $('.start').show();
+  $('.user').hide();
+  $('.jon').hide();
+  $('.result').hide();
+  $('.bumper').hide();
 });
 
 
+$(function showUser() {
+  $('#showUser1').click(function(e) {
+    e.preventDefault()
+    $('.user').show();
+    $('.start').hide();
 
 
+    
+    $( "#slider1" ).slider({
+     value:10,
+     min: 0,
+     max: 500,
+     step: 10,
+     slide: function( event, ui ) {
+       $( "#amount" ).val( "$" + ui.value );
+     }
+    });
+    $( "#amount" ).val( "$" + $( "#slider1" ).slider( "value" ) );
 
-
-
-
-
-$( "#slider1" ).slider({
- value:10,
- min: 0,
- max: 500,
- step: 10,
- slide: function( event, ui ) {
-   $( "#amount" ).val( "$" + ui.value );
- }
+  });
 });
-$( "#amount" ).val( "$" + $( "#slider1" ).slider( "value" ) );
+
+
+$(function showJon() {
+  $('#showJon1').click(function(e) {
+    e.preventDefault()
+    $('.user').hide();
+    $('.jon').show();
+
+    $( "#slider2" ).slider({
+     value:10,
+     min: 0,
+     max: 500,
+     step: 10,
+     slide: function( event, ui ) {
+       $( "#amount" ).val( "$" + ui.value );
+     }
+    });
+    $( "#amount" ).val( "$" + $( "#slider2" ).slider( "value" ) );
 
 
 
-$( "#slider2" ).slider({
- value:10,
- min: 0,
- max: 500,
- step: 10,
- slide: function( event, ui ) {
-   $( "#amount" ).val( "$" + ui.value );
- }
+  });
 });
-$( "#amount" ).val( "$" + $( "#slider2" ).slider( "value" ) );
+
+$(function showResult() {
+  $('#showResult1').click(function(e) {
+    e.preventDefault()
+    $('.result').show();
+    $('.jon').hide();
 
 
 
+    $( "#slider3" ).slider({
+     value:10,
+     min: 0,
+     max: 500,
+     step: 10,
+     slide: function( event, ui ) {
+       $( "#amount" ).val( "$" + ui.value );
+     }
+    });
+    $( "#amount" ).val( "$" + $( "#slider3" ).slider( "value" ) );
 
-$( "#slider3" ).slider({
- value:10,
- min: 0,
- max: 500,
- step: 10,
- slide: function( event, ui ) {
-   $( "#amount" ).val( "$" + ui.value );
- }
+
+  });
 });
-$( "#amount" ).val( "$" + $( "#slider3" ).slider( "value" ) );
 
 
+$(function nextQuestion() {
+  $('#showQuestion2').click(function(e) {
+    e.preventDefault()
+    $('.bumper').show();
+    $('.result').hide();
+
+  });
+});
 
 
-
-
-
-
- });
+});
